5de05f72c225f105efa8aef0c0a843c906d8ea4e
basit <basitmohammed362@gmail.com>
2024-06-13 14:24:37 +0000

    hello

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,17 +1,25 @@
--- create a script to store a procedure 
--- that adds a new correction for a student
--- the procedure must receive 3 arguments:
--- student_id: INT
+-- Creates a stored procedure AddBonus that adds a new
+-- correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE AddBonus(user_id INT,project_name VARCHAR(255),score FLAT)
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
-    DECLARE user_id INT;
-    DECLARE score FLOAT;
-    DECLARE project_name VARCHAR(255);
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_id, score);
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
+    END IF;
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
 END $$
 DELIMITER ;
