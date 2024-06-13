36b1606e76c187a3a2493ac30ac92b880aa156ee
basit <basitmohammed362@gmail.com>
2024-06-13 11:36:31 +0000

    hi

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,7 @@
--- create a user table.
-
-DROP TABLE IF EXISTS USERS;
-
--- create a user table.
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255)
 );
