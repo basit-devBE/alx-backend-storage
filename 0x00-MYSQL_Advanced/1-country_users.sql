aaa43f919530b60463eb60d525f073118eb766d5
basit <basitmohammed362@gmail.com>
2024-06-13 11:13:21 +0000

    hi

--- 0x00-MYSQL_Advanced/1-country_users.sql
+++ 0x00-MYSQL_Advanced/1-country_users.sql
@@ -1,13 +1,7 @@
--- Creates a table with unique users
-
-
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country VARCHAR(50) NOT NULL DEFAULT 'US',
-    CHECK (country IN ('US', 'CO', 'TN'))
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
-
