cb420a78d796ecdadb5ae622606ec340f84c16c8
basit <basitmohammed362@gmail.com>
2024-06-13 14:09:11 +0000

    hello

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,11 +5,10 @@ DELIMITER $$
 CREATE TRIGGER validate_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-BEGIN 
-    IF OLD.email != NEW.email THEN
-        SET NEW.valid.email = 0;
-    ELSE
-        SET NEW.valid.email = NEW.valid.email;
+BEGIN
+    IF NEW.email NOT REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid email';
     END IF;
 END $$
 DELIMITER ;
