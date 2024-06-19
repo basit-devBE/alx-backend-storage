357d794230bf82177ba71c1af934f011497ab96a
basit <basitmohammed362@gmail.com>
2024-06-19 14:59:14 +0000

    hello

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,12 @@ class Cache:
         data = self._redis.get(key)
         return  fn(data) if fn is not None else data
 
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value from a Redis data storage.
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from a Redis data storage.
+        """
+        return self.get(key, lambda x: int(x))
