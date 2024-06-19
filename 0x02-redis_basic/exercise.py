757130b3dc54e6694badda1d92d23439a3f8949e
basit <basitmohammed362@gmail.com>
2024-06-19 14:57:33 +0000

    hello

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,16 +8,21 @@ class Cache:
     """Represents an object for storing data in redis
     """
 
-    def __init__(self)-> None:
+    def __init__(self) -> None:
         """initializes a cache instance
         """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data:Union[str,bytes,int,float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in redis and returns a key
         """
-        key = str(uuid.uuid4())
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-        
+
+     def get(self , key:str, fn: callable(None)) -> Union[str, bytes, int, float]:
+        """Retrieves a value from a redis data storage"""
+        data = self._redis.get(key)
+        return  fn(data) if fn is not None else data
+
