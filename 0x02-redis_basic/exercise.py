5429b087e5e6327564728590684393cbcd662f5f
basit <basitmohammed362@gmail.com>
2024-06-19 14:42:09 +0000

    hello

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python3
 import redis
 import uuid
 from typing import Union
 
+
 class Cache:
-    '''Represents an object for storing data in redis
-    '''
+    """Represents an object for storing data in redis
+    """
 
     def __init__(self)-> None:
-        '''initializes a cache instance
-        '''
+        """initializes a cache instance
+        """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     def store(self, data:Union[str,bytes,int,float]) -> str:
-        '''Stores data in redis and returns a key
-        '''
+        """Stores data in redis and returns a key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
